4dbef0f65b581cfe7c63debe4898d884cfb17199
Otavio <otavio.aragoni@gmail.com>
2017-06-26 23:34:12 -0300

    minor changes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,103 @@
 var map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 40.7413549, lng: -73.9980244},
-		zoom:13
+		center: {lat: -23.561155, lng:-46.731033},
+		zoom:15,
+		styles: [
+			    {
+			        "featureType": "administrative",
+			        "elementType": "all",
+			        "stylers": [
+			            {
+			                "visibility": "on"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "administrative",
+			        "elementType": "labels.text.fill",
+			        "stylers": [
+			            {
+			                "color": "#444444"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "landscape",
+			        "elementType": "all",
+			        "stylers": [
+			            {
+			                "color": "#f2f2f2"
+			            },
+			            {
+			                "visibility": "on"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "poi",
+			        "elementType": "all",
+			        "stylers": [
+			            {
+			                "visibility": "on"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road",
+			        "elementType": "all",
+			        "stylers": [
+			            {
+			                "saturation": -100
+			            },
+			            {
+			                "lightness": 45
+			            },
+			            {
+			                "visibility": "on"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road.highway",
+			        "elementType": "all",
+			        "stylers": [
+			            {
+			                "visibility": "simplified"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road.arterial",
+			        "elementType": "labels.icon",
+			        "stylers": [
+			            {
+			                "visibility": "off"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "transit",
+			        "elementType": "all",
+			        "stylers": [
+			            {
+			                "visibility": "on"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "water",
+			        "elementType": "all",
+			        "stylers": [
+			            {
+			                "color": "#3591b7"
+			            },
+			            {
+			                "visibility": "on"
+			            }
+			        ]
+			    }
+			]
 	});
 
 	var usp = {lat: -23.561155, lng:-46.731033};
@@ -13,3 +108,11 @@ function initMap() {
 		title: 'USP'
 	});
 }
+
+function AppViewModel() {
+    var self = this;
+ 
+    self.title = ko.observable("Neighborhood Map");
+}
+
+ko.applyBindings(new AppViewModel());
