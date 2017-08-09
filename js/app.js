d447807dd10cb909827eb1cc2b0999068dfb8970
Otavio <otavio.aragoni@gmail.com>
2017-08-08 21:36:42 -0300

    add some ko functions, search

--- js/app.js
+++ js/app.js
@@ -1,7 +1,37 @@
+//places oject
+var places = [
+	{
+		"name" : "Instituto de Matematica e Estatistica",
+		"initials" : "IME",
+		"coords" : {"lat" : 0000, "lng": 0000}
+	},
+	{
+		"name" : "Faculdade de Arquitetura e Urbanismo",
+		"initials" : "FAU",
+		"coords" : {"lat" : 000, "lng" : 0000}
+	},
+	{
+		"name" : "Instituto de Quimica",
+		"initials" : "IQ",
+		"coords" : {"lat" : 000, "lng" : 0000}
+	},
+	{
+		"name" : "Escola Politecnica",
+		"initials" : "Poli",
+		"coords" : {"lat" : 000, "lng" : 0000}
+	},
+	{
+		"name" : "Instituto de Biologia",
+		"initials" : "IB",
+		"coords" : {"lat" : 000, "lng" : 0000}
+	}
+	];
+
+
 var map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -23.561155, lng:-46.731033},
+		center: {lat: 0, lng:0},
 		zoom:15,
 		styles: [
 			    {
@@ -102,17 +132,79 @@ function initMap() {
 
 	var usp = {lat: -23.561155, lng:-46.731033};
 
+	var bounds = new google.maps.LatLngBounds();
+
 	var marker = new google.maps.Marker({
 		position: usp,
 		map:map,
 		title: 'USP'
 	});
+
+	bounds.extend(marker.position);
+	map.fitBounds(bounds);
+
+	var infowindow = new google.maps.InfoWindow({
+		content: 'Universidade de Sao Paulo'
+	});
+
+	marker.addListener('click', function(){
+		infowindow.open(map, marker);
+	});
+}
+
+function hasSubstring(string, substring){
+	var lstring = string.toLowerCase();
+	var lsubstring = substring.toLowerCase();
+	if (lstring.indexOf(lsubstring) > -1){
+		return true;
+	}
+	return false;
 }
 
 function AppViewModel() {
     var self = this;
+
+    //self.obsList = ko.observableArray(places);
+
+    function filter(){
+    	var resultsList = [];
+    	for (i = 0; i < places.length; i++){
+    		//if the name or initials contains the search string
+    		if (hasSubstring(places[i].name, self.search()) || hasSubstring(places[i].initials, self.search())) {
+    			//push the object to the new array
+    			resultsList.push(places[i]);
+    		}
+    	}
+    	console.log(resultsList);
+    	return  resultsList;
+	}
  
     self.title = ko.observable("Neighborhood Map");
+    self.search = ko.observable("");
+    
+
+    self.comp = ko.computed(function(){
+    	filter();
+    	return self.search();
+    }, self).extend({ throttle: 800 });
+
+    console.log(places);
+    var list = document.getElementById("results");
+    list.style.display = 'block';
+    for (i = 0; i < places.length; i++){
+    	var newElement = document.createElement("li");
+    	newElement.innerHTML = places[i].initials;
+    	list.appendChild(newElement);
+    }
+
+    //Search in Places API
+   	self.searchPlacesAPI = function () {
+   		var results = document.getElementById("results");
+   		var resultsList;
+
+   		results.innerHTML = resultsList;
+    }
+
 }
 
 ko.applyBindings(new AppViewModel());
