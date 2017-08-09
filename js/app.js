6208ef1465b4cd545f60f720c6a409ea174cb083
Otavio <otavio.aragoni@gmail.com>
2017-08-09 17:51:43 -0300

    infowindows implemented with images

--- js/app.js
+++ js/app.js
@@ -27,6 +27,49 @@ var places = [
 	}
 	];
 var markers = [];
+var url = window.location.href;
+
+$(document).ready(function(){
+	var token = loggedIn(url);
+	if (token){
+		$("#insta").hide();
+		$("#logged").show();
+
+		//ajax request to instagram api
+		$.ajax({
+			url: 'https://api.instagram.com/v1/users/'+userId(token)+'/',
+			dataType: 'jsonp',
+			type: 'GET',
+			data: {access_token: token},
+			success: function(data){
+				$("#logged").append(data.data.username);
+				console.log(data.data.username);
+			},
+			error: function(data){
+				console.log(data);
+			}
+		});
+	}
+});
+
+//verify if the user is logged in on instagram
+//returns the access token or false
+function loggedIn(url){
+	var split = url.split("#");
+	if (split.length > 1){
+		var token = split[1].split("=")[1];
+		return token;
+	}
+	return false;
+}
+
+//returns the user id
+function userId(token){
+	if(token) return token.split('.')[0];
+	return false;
+}
+
+console.log(userId(loggedIn(url)));
 
 var map;
 function initMap() {
@@ -142,9 +185,8 @@ function initMap() {
 		var infowindow = new google.maps.InfoWindow();
 
 		marker.addListener('click', function(){
-			infowindow.open(map, this);
+			populateInfoWindow(this, infowindow);
 		});
-
 		markers.push(marker);
 	}
 
@@ -153,7 +195,61 @@ function initMap() {
 }
 
 function populateInfoWindow(marker, infowindow){
+	// from Project_Code_10_DisplayingRoutesDirectionsService.html on the project github
+	// Check to make sure the infowindow is not already opened on this marker.
+	 if (infowindow.marker != marker) {
+        	// Clear the infowindow content to give the streetview time to load.
+        	infowindow.setContent('');
+        	var content = "<div class='infowindow'>";
+        	for (i = 0; i < 9; i++){
+        		content += '<img src="/img/loading.gif" class="thumbs" id="thumb-'+ i +'">';
+        	}
+        	content += "</div>";
+        	infowindow.setContent(content);
+        	infowindow.marker = marker;
+        	// Make sure the marker property is cleared if the infowindow is closed.
+        	infowindow.addListener('closeclick', function() {
+            	infowindow.marker = null;
+         	});
 
+        	//request the locations for the marker's coords
+        	$.ajax({
+        		url: "https://api.instagram.com/v1/locations/search",
+        		dataType: "jsonp",
+        		type: "GET",
+        		data: {access_token: loggedIn(url), lat: marker.position.lat, lng: marker.position.lng},
+        		success: function(data){
+        			//get the id of the first place in the list
+        			var placeId = data.data[0].id;
+        			//request the recent media of the place
+        			$.ajax({
+        				url: "https://api.instagram.com/v1/locations/" + placeId + "/media/recent",
+        				dataType: "jsonp",
+        				type: "GET",
+        				//had to use a diferent token for authorization purpose
+        				data: {access_token: "327238536.e029fea.868198a8006b4c0bbab397174aa761d5"},
+        				success: function(data){
+        					console.log(data);
+        					for (i = 0; i < 9; i++){
+        						var imgId = "#thumb-" + i;
+        						if (data.data[i]) {
+        							$(imgId).show().attr("src",data.data[i].images.thumbnail.url);
+        						} else {
+        							$(imgId).hide();
+        						}
+        					}
+        				},
+        				error: function(data){
+        					console.log(data);
+        				}
+        			})
+        		},
+        		error: function(data){
+        			console.log(data);	
+        		}
+        	})
+        	infowindow.open(map, marker);
+		}
 }
 
 function hasSubstring(string, substring){
